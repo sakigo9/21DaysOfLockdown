701bd00ae5450fb1cc349bbefea0feaa4ecdc6af
sakigo9 <saketprag322@gmail.com>
2020-04-19 15:43:46 +0530

    commit Day 26

---
+++ Day 26/AcmIpmc.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the acmTeam function below.
+def acmTeam(topic):
+    n=len(topic)
+    com=[]
+    for i in range(n-1):
+        for j in range(i+1,n):
+            l=bin(int(topic[i],2) | int(topic[j],2))
+            com.append(l.count('1'))
+
+    return(max(com),com.count(max(com)))  
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nm = input().split()
+
+    n = int(nm[0])
+
+    m = int(nm[1])
+
+    topic = []
+
+    for _ in range(n):
+        topic_item = input()
+        topic.append(topic_item)
+
+    result = acmTeam(topic)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
